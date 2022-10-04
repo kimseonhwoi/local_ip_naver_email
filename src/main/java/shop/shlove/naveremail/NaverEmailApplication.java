e2268c0aa349362f0d1b48873d8c503ccd9ec5bf
shkim <shkim@gsitm.com>
2022-10-04 16:14:01 +0900

    초기세팅

---
+++ src/main/java/shop/shlove/naveremail/NaverEmailApplication.java
@@ -0,0 +1,13 @@
+package shop.shlove.naveremail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaverEmailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shop/shlove/naveremail/NaverEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.shlove.naveremail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
